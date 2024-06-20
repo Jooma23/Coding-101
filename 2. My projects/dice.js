da739f155b91fe66d9ef7328a718e84ae87ae7f3
Ali Jooma <23j07a@gmail.com>
2024-06-20 09:50:39 -0700

    adding dice game skeleton

--- 2. My projects/dice.js
+++ 2. My projects/dice.js
@@ -1,1 +1,10 @@
-document.onload
+/* 
+
+Steps to take: 
+1. Add static die upon "load"
+2. Calculate the winner and draw
+3. Add an element to display the winner
+4. Change static > random
+5. Change "load" to button 
+x. Clear the dice to start
+
