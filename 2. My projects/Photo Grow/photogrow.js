a43bf09a2e09d7846ff22c28d3c76a8858fb7aa9
Ali Jooma <23j07a@gmail.com>
2024-06-21 15:52:53 -0700

    adding event listeners to all, returning id, basic styling

--- 2. My projects/Photo Grow/photogrow.js
+++ 2. My projects/Photo Grow/photogrow.js
@@ -2,8 +2,32 @@
 
 /*
 
-> Put event listeners on all boxes (click);
-> When a box is clicked, return it's repective #id;
-> print the id to the console.log;
-> add styling to that id and general styling;
-> play with styling;
+> Put event listeners on all boxes (click); - done
+> When a box is clicked, return it's repective #id; - done
+> print the id to the console.log; - done
+> add styling to that id; - done
+> play with general and specific styling;
+
+*/
+
+// To add event listener on all items
+for (var i = 0; i < document.querySelectorAll(".box").length; i++) {
+    document.querySelectorAll(".box")[i].addEventListener("click", testingEventClicks);
+}
+
+// Event listener fn; prints id number  
+function testingEventClicks() {
+    window.boxClicked = this.id;
+    console.log(boxClicked + " should change to white");
+    changeColor();
+} 
+
+// Changes the color
+function changeColor() {
+    var boxToChange = document.getElementById(boxClicked);
+    // boxToChange.style.color = "white";
+    boxToChange.style.transition = "all 2s ease-out";
+}
+
+
+
