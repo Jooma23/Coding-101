0787607cae9e5f6ea6b5c64ecb5ace8deafc7329
Ali Jooma <23j07a@gmail.com>
2024-06-24 20:52:29 -0700

    adding button toggling js and basic pills

--- 2. My projects/Voting Project/voting.js
+++ 2. My projects/Voting Project/voting.js
@@ -1,1 +1,19 @@
-//alert("hello world");
+let functionPost = document.getElementById("postButton"); 
+let btnToggler = document.querySelector(".topbox"); 
+
+
+//Adding the event listener
+functionPost.addEventListener("click", popupToggler);
+
+    function popupToggler() {
+        if (btnToggler.classList.contains("hidden")) {
+            btnToggler.classList.remove("hidden");
+            functionPost.textContent = "X";
+        }
+        else {
+        btnToggler.classList.add("hidden");
+        functionPost.textContent = "+";   
+    }
+}
+
+
