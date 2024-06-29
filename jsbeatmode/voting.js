3618715baa55cefa1a6625893af5348306cbe190
Ali Jooma <23j07a@gmail.com>
2024-06-29 08:41:48 -0700

    more refactoring for better html structure

--- jsbeatmode/voting.js
+++ jsbeatmode/voting.js
@@ -1,14 +1,13 @@
 let functionPost = document.getElementById("postButton"); 
 let btnToggler = document.querySelector(".topbox"); 
 
-
 //Adding the event listener
 functionPost.addEventListener("click", popupToggler);
 
     function popupToggler() {
         if (btnToggler.classList.contains("hidden")) {
-            btnToggler.classList.remove("hidden");
-            functionPost.textContent = "X";
+                btnToggler.classList.remove("hidden");
+                functionPost.textContent = "X";
         }
         else {
         btnToggler.classList.add("hidden");
@@ -16,4 +15,6 @@ functionPost.addEventListener("click", popupToggler);
     }
 }
 
-
+/* Problems: 
+I have no idea why pressing "submit" closes the box
+*/
