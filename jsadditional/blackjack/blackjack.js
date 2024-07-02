9068497978cd0a6e789cbe353d8460f211d69bb2
Ali Jooma <23j07a@gmail.com>
2024-07-02 11:06:01 -0700

    styling done and js gameplan ready

---
+++ jsadditional/blackjack/blackjack.js
@@ -0,0 +1,15 @@
+/* JS plan 
+
+- Add event listeners on all buttons
+
+- Learn how to use an array
+- hit()
+- add a card 3 and card 4
+- 
+
+- stay()
+> dealers turn: for loop until they get to 17, then end
+> print results of winner or tie
+
+- new game()
+> Display random numbers in div
