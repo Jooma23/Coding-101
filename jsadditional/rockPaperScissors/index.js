d5681679755a73f71a3c6c6438fccccfbbe18225
Ali Jooma <23j07a@gmail.com>
2024-07-09 10:26:23 -0700

    rock, paper, scissors is done

--- jsadditional/rockPaperScissors/index.js
+++ jsadditional/rockPaperScissors/index.js
@@ -1,6 +1,7 @@
 let fighters = ["🪨", "📜", "✂️"] 
 
 let userstageEl = document.getElementById("userstage")
+let winnerText = document.querySelector("p")
 let cpustageEl = document.getElementById("cpustage")
 
 let rockButton = document.getElementById("rockButton")
@@ -10,21 +11,45 @@ let scissorsButton = document.getElementById("scissorsButton")
 function computerChoice() {
     let compChoice = (Math.floor(Math.random() * fighters.length));
     cpustageEl.textContent = fighters[compChoice];
+    
+    
+    if (userstageEl.textContent === cpustageEl.textContent) {
+        winnerText.innerText = "It's a tie!"
+    }
+    else if (userstageEl.textContent === "🪨" && cpustageEl.textContent === "✂️")
+        {
+        winnerText.innerText = "You won!"
+    }
+    else if (userstageEl.textContent === "📜" && cpustageEl.textContent === "🪨")
+        {
+        winnerText.innerText = "You won!"
+    }
+    else if (userstageEl.textContent === "✂️" && cpustageEl.textContent === "📜")
+        {
+        winnerText.innerText = "You won!!"
+    }
+    else {
+        winnerText.innerText = "CPU wins!"
+    }
 }
+
 
 rockButton.addEventListener("click", function() {
     userstageEl.textContent = fighters[0];
-    computerChoice()
+    computerChoice();
+    return userstageEl.textContent;
 })
 
 paperButton.addEventListener("click", function() {
     userstageEl.textContent = fighters[1];
-    computerChoice()
+    computerChoice();
+    return userstageEl.textContent;
 })
 
 scissorsButton.addEventListener("click", function() {
     userstageEl.textContent = fighters[2];
-    computerChoice()   
+    computerChoice();
+    return userstageEl.textContent;   
 })
 
 // Build a check winner function
